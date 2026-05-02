6db840fb94189fd1d1319302af4c566114dac932
ManasGeedkar <geedkarmanas@gmail.com>
2026-05-02 21:34:08 +0530

    glare.py

--- glare.py
+++ glare.py
@@ -1,3 +1,20 @@
+# import numpy as np
+
+# class GlareDetector:
+#     def __init__(self, spike_threshold=50.0):
+#         self.prev_brightness = 100.0
+#         self.spike_threshold = spike_threshold
+        
+#     def detect(self, gray_frame):
+#         current_brightness = np.mean(gray_frame)
+#         is_glare = False
+        
+#         if current_brightness > self.prev_brightness + self.spike_threshold:
+#             is_glare = True
+            
+#         self.prev_brightness = current_brightness
+#         return is_glare
+
 import numpy as np
 
 class GlareDetector:
@@ -14,3 +31,4 @@ class GlareDetector:
             
         self.prev_brightness = current_brightness
         return is_glare
+
